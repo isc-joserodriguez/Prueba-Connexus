afbb4d4b080074b208bfdab630025fe8416c9a92
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-10-16 17:44:27 -0600

    Agregados los servicios a la API

---
+++ P2/angular-app/src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const apiUrl = `${environment.urlMongo}personas`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(credenciales) {
+    return this.http.post(`${apiUrl}/login`, credenciales);
+  }
+
+  signup(datos) {
+    return this.http.post(`${apiUrl}/signup`, datos);
+  }
+
+  verificarToken(token) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: token
+      })
+    };
+
+    return this.http.get(`${apiUrl}/verificarSesion`, httpOptions);
+  }
+}

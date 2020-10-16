b90273fab4f01c7f3ed51224cc76a69201906386
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-10-16 17:17:39 -0600

    Agregados componentes Home, Login & Signup

--- P2/angular-app/src/app/app.module.ts
+++ P2/angular-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SingupComponent } from './singup/singup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SingupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
